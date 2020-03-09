74ef4d7258822f321e608822c20194c1877872a1
Andreas Korb <andreas.d.korb@gmail.com>
2020-03-09 17:15:50 +0100

    Improved reference script

--- start_packet_viewer.py
+++ start_packet_viewer.py
@@ -1,13 +1,10 @@
 from scapy.contrib.cansocket_python_can import PythonCANSocket
-
 from scapy.contrib.cansocket_native import CANSocket
-
-from scapy.contrib.isotp import ISOTPSocket
-
-
+from scapy.contrib.isotp import ISOTPNativeSocket as ISOTPSocket
 from scapy.tools.packet_viewer.viewer import viewer
 
-#socket = PythonCANSocket(bustype='virtual', channel="vcan1")
+# socket = ISOTPSocket("vcan0", did=0x123, sid=0x456)
+# socket = PythonCANSocket(bustype='virtual', channel="vcan1")
 socket = CANSocket("can0")
 viewer(socket)
 socket.close()
