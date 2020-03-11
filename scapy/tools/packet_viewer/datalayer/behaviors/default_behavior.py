d578f64e98b0e63d6972dcada1328b105ee2c993
Andreas Korb <andreas.d.korb@gmail.com>
2020-03-11 13:54:27 +0100

    Standardize column specification

--- scapy/tools/packet_viewer/datalayer/behaviors/default_behavior.py
+++ scapy/tools/packet_viewer/datalayer/behaviors/default_behavior.py
@@ -1,18 +1,23 @@
-from abc import ABC
-
 from scapy.packet import Packet
 from scapy.supersocket import SuperSocket
 
 
-class DefaultBehavior(ABC):
+class DefaultBehavior:
     # Do NOT remove the 'socket' parameter. All constructors of all behaviors have to be 'call-compatible'
     # Also the 'additional_columns' parameter is usually used by sub classes, but not restricted to it.
     def __init__(self, socket: SuperSocket, basecls, additional_columns: list = None):
         self.counter = 0
         # Tuple: Name, length of column, function to get the value from (lambda p: p.identifier)
         self.basecls = socket.basecls if hasattr(socket, 'basecls') else basecls
-        self.additional_columns: list = additional_columns or []
         self.packets = {}
+        self.columns = [('TIME', 20, lambda p: str(p.time)),
+                        ('LENGTH', 5, lambda p: str(len(p)))]
+
+        self.columns += additional_columns
+
+        self.format_string = ""
+        for (name, length, _) in self.columns:
+            self.format_string += "{" + name + ":<" + str(length) + "}"
 
     def get_data(self, packet):
         # bytearray is important for Py2 compatibility
@@ -20,45 +25,22 @@ class DefaultBehavior(ABC):
 
     def get_header(self) -> str:
         cols: dict = dict()
-        for (name, _, _) in self.additional_columns:
+        for (name, _, _) in self.columns:
             cols[name] = name.upper()
 
-        for field in self.basecls.fields_desc:
-            cols[field.name] = field.name.upper()
-
-        return self._format_strings(time="TIME", **cols)
+        return self._format_strings(**cols)
 
     def get_group(self, packet):
-        # by default all packets are considered to be in the same group
+        # by default all packets with the same length are considered to be in the same group
         length = len(packet)
         return length
 
     def _format_strings(self, **kwargs):
-        template: str = self._get_format_string()
-        for key, value in kwargs.items():
-            kwargs[key] = str(value)
-        return template.format(**kwargs)
-
-    def _get_format_string(self) -> str:
-        # First column is TIME
-        # space for cursor
-        format_str = "{time:<20}"
-
-        obj = self.basecls()
-        obj.build()
-
-        # Now add additional columns
-        for (name, length, _) in self.additional_columns:
-            format_str += "{" + name + ":<" + str(length) + "}"
-
-        for field in self.basecls.fields_desc:
-            format_str += "{" + field.name + ":<" + str(obj.fields[field.name]) + "}"
-
-        return format_str
+        return self.format_string.format(**kwargs)
 
     def get_packet_formatted(self, packet: Packet):
-        cols: dict = packet.fields.copy()
-        for (name, _, fun) in self.additional_columns:
+        cols: dict = dict()
+        for (name, _, fun) in self.columns:
             cols[name] = fun(packet)
 
-        return self._format_strings(time=packet.time, **cols)
+        return self._format_strings(**cols)
