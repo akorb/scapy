4009e89049c9a675f2bd6e2c04ce698ec1c3b45a
Andreas Korb <andreas.d.korb@gmail.com>
2020-03-11 15:34:03 +0100

    Refactoring

--- start_packet_viewer.py
+++ start_packet_viewer.py
@@ -4,13 +4,13 @@ from scapy.contrib.cansocket_native import CANSocket
 from scapy.contrib.isotp import ISOTPNativeSocket as ISOTPSocket
 from scapy.layers.can import CAN
 from scapy.layers.l2 import Ether
-from scapy.tools.packet_viewer.viewer import get_can_predefined, get_isotp_predefined, viewer
+from scapy.tools.packet_viewer.viewer import get_can_preset, get_isotp_preset, viewer
 
 socket = ISOTPSocket("vcan0", did=0x123, sid=0x456)
 # socket = PythonCANSocket(bustype='virtual', channel="vcan1")
 # socket = CANSocket("vcan0")
 # socket = L2Socket("enp4s0")
-viewer(socket, basecls=socket.basecls, **get_isotp_predefined(socket))
-# viewer(socket, basecls=socket.basecls, **get_can_predefined())
+viewer(socket, basecls=socket.basecls, **get_isotp_preset(socket))
+# viewer(socket, basecls=socket.basecls, **get_can_preset())
 # viewer(socket, Ether)
 socket.close()
