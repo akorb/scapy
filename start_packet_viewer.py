53edec3a81171792252e0e25de8d7e12e16d9032
Andreas Korb <andreas.d.korb@gmail.com>
2020-03-20 19:14:45 +0100

    Readded start script

--- start_packet_viewer.py
+++ start_packet_viewer.py
@@ -1,26 +1,16 @@
-import threading
-import time
-
+from scapy.arch import L2Socket
 from scapy.contrib.cansocket_python_can import PythonCANSocket
+from scapy.contrib.cansocket_native import CANSocket
+from scapy.contrib.isotp import ISOTPNativeSocket as ISOTPSocket
 from scapy.layers.can import CAN
-from scapy.tools.packet_viewer.viewer import viewer, get_can_preset, get_python_can_preset
+from scapy.layers.l2 import Ether
+from scapy.tools.packet_viewer.viewer import get_can_preset, get_isotp_preset, viewer
 
-
-def send_msg():
-    socket2 = PythonCANSocket(bustype='virtual', channel="vcan1")
-
-    for i in range(10):
-        socket2.send(CAN(identifier=0x1001, data='test'))
-        time.sleep(1)
-
-
-# socket = ISOTPSocket("can0", did=0x123, sid=0x456)
-socket = PythonCANSocket(bustype='virtual', channel="vcan1")
-thread1 = threading.Thread(target=send_msg)
-thread1.start()
-
-viewer(socket, basecls=socket.basecls, **get_python_can_preset())
-
-thread1.join()
-
+socket = ISOTPSocket("vcan0", did=0x123, sid=0x456)
+# socket = PythonCANSocket(bustype='virtual', channel="vcan1")
+# socket = CANSocket("vcan0")
+# socket = L2Socket("enp4s0")
+viewer(socket, basecls=socket.basecls, **get_isotp_preset())
+# viewer(socket, basecls=socket.basecls, **get_can_preset())
+# viewer(socket, Ether)
 socket.close()
