27856989ef8114736da61da218d20b9d4bffbf5f
akorb <andreas.d.korb@gmail.com>
2020-03-26 12:51:55 +0100

    Unify test scripts

--- test_packet_viewer.py
+++ test_packet_viewer.py
@@ -3,14 +3,14 @@ import time
 
 from scapy.contrib.cansocket_python_can import PythonCANSocket
 from scapy.layers.can import CAN
-from scapy.tools.packet_viewer.viewer import viewer, get_can_preset, get_python_can_preset
+from scapy.tools.packet_viewer.viewer import viewer, get_can_preset, get_can_preset_py2
 
 
 def send_msg():
     socket2 = PythonCANSocket(bustype='virtual', channel="vcan1")
 
     for i in range(10):
-        socket2.send(CAN(identifier=0x1001, data='test'))
+        socket2.send(CAN(identifier=0x1001, data="test" + str(i)))
         time.sleep(1)
 
 
@@ -19,7 +19,7 @@ socket = PythonCANSocket(bustype='virtual', channel="vcan1")
 thread1 = threading.Thread(target=send_msg)
 thread1.start()
 
-viewer(socket, basecls=socket.basecls, **get_python_can_preset())
+viewer(socket, basecls=socket.basecls, **get_can_preset_py2())
 
 thread1.join()
 
