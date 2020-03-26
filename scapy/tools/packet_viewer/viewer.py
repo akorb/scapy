df18b7c4707c787a56d9d977d11fc619802fed08
Nils Weiss <nils@we155.de>
2020-03-26 16:16:15 +0100

    fix flake8

--- scapy/tools/packet_viewer/viewer.py
+++ scapy/tools/packet_viewer/viewer.py
@@ -1,32 +1,32 @@
 from typing import List, Union, Tuple, Dict, Callable
 from urwid import AttrMap, MainLoop
 
-from scapy.supersocket import SuperSocket
 from scapy.compat import bytes_hex
 from scapy.tools.packet_viewer.viewlayer.views.main_view import MainWindow
 
 
 palette = [
-        ("default", "light gray", "black"),
-        ("header", "light blue", "black"),
-        ("packet_view_header", "light cyan", "black"),
-        ("focused", "light green", "black"),
-        ("unfocused", "black", "black"),
-        ("reversed", "standout", "black"),
-        ("green", "dark green", "black"),
-        ("red", "dark red", "black"),
-        ("bold", "light gray,bold", "black"),
-        ("bold-blue", "light blue,bold", "black"),
-        ("green", "light green", "black"),
-        ("bold-yellow", "yellow, bold ", "black"),
-        ("bold-orange", "", "black", "", "", "#f80"),
-        ("bold-red", "dark red, bold ", "black"),
-        ("bg background", "light gray", "black"),
-        ("bg 1", "black", "dark blue", "standout"),
-        ("bg 2", "black", "dark cyan", "standout"),
-        ("bg 1 line", "dark red", "dark blue"),
-        ("bg 2 line", "dark red", "dark cyan"),
-    ]  # type: List[Union[Tuple[str, str, str],Tuple[str, str, str, str], Tuple[str, str, str, str, str, str] ]]
+    ("default", "light gray", "black"),
+    ("header", "light blue", "black"),
+    ("packet_view_header", "light cyan", "black"),
+    ("focused", "light green", "black"),
+    ("unfocused", "black", "black"),
+    ("reversed", "standout", "black"),
+    ("green", "dark green", "black"),
+    ("red", "dark red", "black"),
+    ("bold", "light gray,bold", "black"),
+    ("bold-blue", "light blue,bold", "black"),
+    ("green", "light green", "black"),
+    ("bold-yellow", "yellow, bold ", "black"),
+    ("bold-orange", "", "black", "", "", "#f80"),
+    ("bold-red", "dark red, bold ", "black"),
+    ("bg background", "light gray", "black"),
+    ("bg 1", "black", "dark blue", "standout"),
+    ("bg 2", "black", "dark cyan", "standout"),
+    ("bg 1 line", "dark red", "dark blue"),
+    ("bg 2 line", "dark red", "dark cyan"),
+]  # type: List[Union[Tuple[str, str, str],Tuple[str, str, str, str], Tuple[str, str, str, str, str, str] ]]  # noqa: E501
+
 
 def viewer(*args, **kwargs):
     main_window = \
@@ -40,7 +40,7 @@ def viewer(*args, **kwargs):
 def get_isotp_preset():
     # type: (...) -> Dict[str, List[Tuple[str, int, Callable]]]
     return {"columns": [("SRC", 6, lambda p: format(p.src, "03X")),
-                        ("DST", 6, lambda p: format(p.dst, "03X")),]}
+                        ("DST", 6, lambda p: format(p.dst, "03X"))]}
 
 
 # TODO: This show Identifier(integer?) and ID(hex)
@@ -49,5 +49,5 @@ def get_can_preset():
     return {
         "columns": [("ID", 8, lambda p: format(p.identifier, "03X"))],
         "get_group": lambda p: p.identifier,
-        "get_data": lambda p: bytes_hex(p.data),
+        "get_data": lambda p: bytes_hex(p.data)
     }
